1c400772ed517ecb43693194b8c976b8a40c037f
anton@gateway.fm <anton@MacBook-Pro-3.fritz.box>
2023-12-18 11:20:16 +0100

    use DownloadL2Blocks instead of DownloadAllL2BlocksToChannel

--- zk/test/datastream_test.go
+++ zk/test/datastream_test.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"os"
 	"strconv"
 	"testing"
 
 	"github.com/ledgerwatch/erigon/zk/datastream"
-	"github.com/ledgerwatch/erigon/zk/datastream/client"
-	"github.com/ledgerwatch/erigon/zk/datastream/types"
 )
 
 func generateTestCases(maxBlock uint64, blockIncrement uint64) map[string]struct {
@@ -62,77 +58,27 @@ func TestDatastream(t *testing.T) {
 	var testCases = generateTestCases(maxBlock, blockIncrement)
 	for test_name, tc := range testCases {
 		tc := tc
-		tn := test_name
-
-		blockChan := make(chan types.FullL2Block)
-		gerUpdateChan := make(chan types.GerUpdate)
 
 		t.Run(test_name, func(t *testing.T) {
+
 			t.Parallel()
 
-			go func() {
-				for {
-					if _, _, err := datastream.DownloadAllL2BlocksToChannel(streamUrl, blockChan, gerUpdateChan,
-						tc.fromBlock); err != nil {
-						if errors.Is(err, client.ErrBadBookmark) {
-							t.Fail()
-							t.Logf("Bad Bookmark")
-							break
-						}
-					}
+			l2Blocks, _, _, _, _ := datastream.DownloadL2Blocks(streamUrl, tc.fromBlock, int(blockIncrement))
+
+			var expectedBlockNumber uint64 = tc.fromBlock
+			var missingBlocks []uint64
+
+			for _, l2Block := range *l2Blocks {
+				if expectedBlockNumber != l2Block.L2BlockNumber {
+					missingBlocks = append(missingBlocks, l2Block.L2BlockNumber)
+
 				}
-			}()
-
-			receivedBlocks := make(map[uint64]types.FullL2Block)
-
-			for {
-				select {
-
-				case l2Block, ok := <-blockChan:
-					if !ok {
-						t.Fail()
-						t.Logf("L2 Block channel closed")
-						break
-					}
-					if l2Block.L2BlockNumber >= tc.toBlock {
-						fmt.Printf("%s received all blocks - checking!\n", tn)
-						missingBlocks := checkContiguousBlocks(receivedBlocks, tc.toBlock, tc.fromBlock)
-						if len(missingBlocks) != 0 {
-							t.Fail()
-							t.Logf("There are missing blocks: %d", missingBlocks)
-						}
-						return
-					}
-					receivedBlocks[l2Block.L2BlockNumber] = l2Block
-					if l2Block.L2BlockNumber%10000 == 0 {
-						fmt.Printf("%s received block %d\n", tn, l2Block.L2BlockNumber)
-					}
-
-				case gerUpdate, ok := <-gerUpdateChan:
-					if !ok {
-						log.Println("GER Update channel closed")
-					}
-					_ = gerUpdate
-					// Handle GER   if necessary
-				}
+				expectedBlockNumber++
+			}
+			if len(missingBlocks) != 0 {
+				t.Fail()
+				t.Logf("There are missing blocks: %d", len(missingBlocks))
 			}
 		})
 	}
-}
-
-func checkContiguousBlocks(receivedBlocks map[uint64]types.FullL2Block, toBlockNumber uint64, fromBlockNumber uint64) []uint64 {
-	var expectedBlockNumber uint64 = fromBlockNumber
-	var missingBlocks []uint64
-
-	for {
-		if expectedBlockNumber >= toBlockNumber {
-			break
-		}
-		if _, ok := receivedBlocks[expectedBlockNumber]; !ok {
-			missingBlocks = append(missingBlocks, expectedBlockNumber)
-
-		}
-		expectedBlockNumber++
-	}
-	return missingBlocks
 }
